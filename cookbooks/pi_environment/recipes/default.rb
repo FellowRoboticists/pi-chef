b18adf17b822973585a9fdade35d97c693b51dd3
Dave Sieh <dj0hnve@gmail.com>
2013-03-12 19:30:41 -0600

    Added the pi_environment cookbook.

---
+++ cookbooks/pi_environment/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: pi_environment
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "sqlite3" do
+end
+
+package "emacs" do
+end
+
+package "arduino" do
+end
+
+package "fswebcam" do
+end
+
+package "tcl" do
+end
+
+package "git" do
+end
--- roles/rpi_base.rb
+++ roles/rpi_base.rb
@@ -2,5 +2,6 @@ name "rpi_base"
 description "Base role to be applied to all Raspberry Pi instances"
 run_list(
          "recipe[apt]",
-         "recipe[users::sysadmins]"
+         "recipe[users::sysadmins]",
+         "recipe[pi_environment]"
 )
